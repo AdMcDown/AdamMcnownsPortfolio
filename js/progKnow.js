f42b24fb4c30d534a975d637362c9a96bc4d20be
Adam McNown <adammcnown@gmail.com>
2020-09-02 16:48:26 -0400

    cleaned up sections for knowledge page

--- js/progKnow.js
+++ js/progKnow.js
@@ -14,3 +14,11 @@ function topOfPage() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// function hashHandler() {
+//     console.log('The hash has changed!');
+//     window.location.hash = this.hash;
+//     window.scroll(0, -50);
+// }
+
+// window.addEventListener('hashchange', hashHandler, false);
